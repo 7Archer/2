5adb6919150b0cb30e1abf416a0e237534c0ca6f
7Archer <1351346287@qq.com>
2022-03-15 23:20:55 +0800

    解析抓取朴朴数据

--- main.py
+++ main.py
@@ -13,11 +13,11 @@ def say():
     url='https://j1.pupuapi.com/client/product/storeproduct/detail/deef1dd8-65ee-46bc-9e18-8cf1478a67e9/714c63f8-075d-4d20-b69c-246ac48d9da1'
     res = requests.get(url, headers=head)
     dict1 = json.loads(res.text)
-    print('----------'+'商品:'+dict1['data']['name']+'----------')
-    print('规格:' + dict1['data']['spec'])
-    print('价格:' + str(int(dict1['data']['price'])/100))
-    print('原价/折扣价:' + str(int(dict1['data']['market_price'])/100)+'/'+str(int(dict1['data']['price'])/100))
-    print('详细内容:' + dict1['data']['share_content'])
+    print('----------'+'商品:'+dict1['data']['name']+'----------') #商品名
+    print('规格:' + dict1['data']['spec']) #规格
+    print('价格:' + str(int(dict1['data']['price'])/100)) #价格
+    print('原价/折扣价:' + str(int(dict1['data']['market_price'])/100)+'/'+str(int(dict1['data']['price'])/100)) #原价与折扣价
+    print('详细内容:' + dict1['data']['share_content']) #详细内容
     print('')
     print('----------'+'"'+ dict1['data']['name'] +'"'+'的价格波动'+ '----------')
     try:
